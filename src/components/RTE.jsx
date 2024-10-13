f7c7d7cc22d1a84baaa4df8e0b4bcdb104ebe4b1
unknown <sweetyishamittal@gmail.com>
2024-10-14 02:53:11 +0530

    my backend data

---
+++ src/components/RTE.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Editor} from '@tinymce/tinymce-react'
+
+export default function RTE() {
+  return (
+   <Editor
+   initialValue='default value'
+   init={
+    {
+        branding: false,
+        height:500,
+        menubar:true,
+    }
+   }
+   />
+  )
+}
+
