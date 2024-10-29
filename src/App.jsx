ae7eaedf93489e51850b2ac15f1d8722baf6c38e
unknown <sweetyishamittal@gmail.com>
2024-10-29 22:45:37 +0530

    correction fix

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ useEffect(() =>{
       <div className='w-full block'>
       <Header/>
       <main>
-      Todo: {/* <outlet/> */}
+      Todo: <Outlet/>
       </main>
       <Footer/>
       </div>
