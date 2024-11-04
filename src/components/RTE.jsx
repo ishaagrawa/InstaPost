2bc9a71e7a4200adef452de2ba7590cab3a8bc59
unknown <sweetyishamittal@gmail.com>
2024-11-05 01:19:27 +0530

    Editor Id name changed

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
        control={control}
       render={({field: {onChange}}) => (
          <Editor 
-         apiKey={import.meta.env.VITE_EDITOR_KEY }  
+         apiKey={import.meta.env.VITE_EDITOR_ID}  
          initialValue={defaultValue}
          init={{
           initialValue: defaultValue,
